d03d15a4a94888d0b7a10b6c2ef374e37a76efd8
Tyler Newman <tylerjnewman18@gmail.com>
2020-08-25 15:58:37 -0700

    Fix width on desktop to 1270px

--- src/components/Midsection.js
+++ src/components/Midsection.js
@@ -5,15 +5,15 @@ import ProductCard from './ProductCard'
 
 const Section = ({ children }) => <div className='section'>{children}</div>
 const GridContainer = ({ children }) => (
-  <div className='container has-text-centered py-4'>{children}</div>
+  <div className='container has-text-centered py-4' style={{ maxWidth: 1270 }}>
+    {children}
+  </div>
 )
 const Grid = ({ children }) => (
   <div className='columns is-multiline pb-3'>{children}</div>
 )
 const Column = ({ children }) => (
-  <div className='column is-3-desktop is-4-tablet '>
-    {children}
-  </div>
+  <div className='column is-3-desktop is-4-tablet '>{children}</div>
 )
 
 const Midsection = () => (
