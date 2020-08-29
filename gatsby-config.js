6b87539efdd6dbcdc2d528f5abfe904a4da3bd12
Tyler Newman <tylerjnewman18@gmail.com>
2020-08-28 20:15:32 -0700

    commit everything

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,33 @@ module.exports = {
     siteUrl: 'https://www.cannabunny.co'
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-wordpress-experimental',
+      options: {
+        url:
+          process.env.WPGRAPHQL_URL ||
+          'https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql',
+        verbose: true,
+        develop: {
+          hardCacheMediaFiles: true
+        },
+        debug: {
+          graphql: {
+            writeQueriesToDisk: true
+          }
+        },
+        type: {
+          Post: {
+            limit:
+              process.env.NODE_ENV === 'development'
+                ? // Lets just pull 50 posts in development to make it easy on ourselves.
+                50
+                : // and we don't actually need more than 5000 in production for this particular site
+                5000
+          }
+        }
+      }
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
